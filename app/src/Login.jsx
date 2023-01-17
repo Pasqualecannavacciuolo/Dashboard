b7785c493e1102a8d8685b8957974aedf5198035
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2023-01-17 20:28:33 +0100

    Allineamento modifiche di stile

--- app/src/Login.jsx
+++ app/src/Login.jsx
@@ -125,8 +125,8 @@ const Login = () => {
                     </p>
                 </div>
                 <form onSubmit={handleSubmit}>
-                    <img className="mb-4" src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
-                    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+                <svg width="64px" height="64px" viewBox="0 0 32 32" id="icon" xmlns="http://www.w3.org/2000/svg" fill="#0E79B2"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><title>logo--medium</title><path className="cls-blue" d="M4,4V28H28V4ZM23.9385,9.6865,22.6514,10.92a.3766.3766,0,0,0-.1431.3613v9.0674a.3765.3765,0,0,0,.1431.3613l1.257,1.2339v.271h-6.323v-.271L18.8877,20.68c.1279-.128.1279-.1656.1279-.3609V12.99l-3.62,9.1958H14.906L10.6907,12.99v6.1631a.8505.8505,0,0,0,.2334.7071l1.6936,2.0547v.2709H7.8154v-.2709L9.509,19.86a.82.82,0,0,0,.2183-.7071V12.0264A.6231.6231,0,0,0,9.5239,11.5L8.0186,9.6865v-.271h4.6743l3.613,7.9239,3.1765-7.9239h4.4561Z"></path><path id="inner-path" className="cls-2" d="M9.7273,12.0266A.6246.6246,0,0,0,9.524,11.5L8.0186,9.6863V9.4154H12.693l3.613,7.9238,3.1764-7.9238h4.4561v.2709L22.6513,10.92a.3763.3763,0,0,0-.143.3612v9.0676a.3763.3763,0,0,0,.143.3612l1.2571,1.2341v.2709H17.5856v-.2709L18.8878,20.68c.1279-.1279.1279-.1656.1279-.3612V12.99l-3.62,9.1955h-.4893L10.6907,12.99v6.1629a.8506.8506,0,0,0,.2334.7074l1.6936,2.0543v.2709H7.8154v-.2709L9.509,19.86a.82.82,0,0,0,.2183-.7074Z"></path><rect id="_Transparent_Rectangle_" data-name="<Transparent Rectangle>" className="cls-2" width="32" height="32"></rect></g></svg>                            
+                    <h1 className="h3 mb-3 fw-normal">Effettua il Login</h1>
 
                     <div className="form-floating">
                         <input
@@ -202,7 +202,7 @@ const Login = () => {
                     </div>
                     
                     <button
-                        className="w-100 btn btn-lg btn-primary mt-3" type="submit"
+                        className="w-100 btn btn-lg submit-button mt-3" type="submit"
                         disabled={!validName || !validPwd ? true : false}
                     >
                         Sign in
