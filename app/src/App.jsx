61f43ee1c17d1c5dac01c255d5157a1d4ea31e9f
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2023-01-22 14:12:26 +0100

    Aggiunto scroll to top per il routing

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,24 +1,28 @@
 import "./App.css";
-import Register from "./Register";
+
 import { Route, Routes } from "react-router-dom";
-//import logo from "./logo.svg";
+
+import Register from "./Register";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import OrderDetail from "./pages/OrderDetail";
 import HomeDashboard from "./pages/HomeDashboard";
+import ScrollToTop from "./ScrollToTop";
+
 
 function App() {
   return (
     <div className="App" role="main">
+      <ScrollToTop>
       <Routes>
-        <Route path='/' element={<Register />} />
-        <Route path='login' element={<Login />} />
-        <Route path='dashboard/*' element={<Dashboard />} >
-            <Route index element={<HomeDashboard />} />
-            <Route path="order/:orderId" element={<OrderDetail />} />
-        </Route>
+          <Route path='/' element={<Register />} />
+          <Route path='login' element={<Login />} />
+          <Route path='dashboard/*' element={<Dashboard />} >
+              <Route index element={<HomeDashboard />} />
+              <Route path="order/:orderId" element={<OrderDetail />} />
+          </Route>
       </Routes>
-      
+      </ScrollToTop>
     </div>
   );
 }
