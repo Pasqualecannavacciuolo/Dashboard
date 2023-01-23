d31797ed8a9e5db14547ee75c9178a0504eaa0a9
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2023-01-23 15:55:13 +0100

    Ignore

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const jwt = require("jsonwebtoken");
 const app = express();
 
 const corsOptions = {
-    origin: '*',
+    origin: 'http://localhost:3000',
     optionsSuccessStatus: 200,
     credentials: true
 }
