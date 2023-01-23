9953bd87565892e61524d5ef981d000bd555729b
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2023-01-23 21:32:54 +0100

    Aggiunta di Supabase

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 
-const db = require("./queries");
+const { createClient } = require("@supabase/supabase-js");
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
@@ -12,15 +17,6 @@ const corsOptions = {
     optionsSuccessStatus: 200,
     credentials: true
 }
-
-const Pool = require("pg").Pool
-const pool = new Pool({
-  user: 'pasquale',
-  host: 'localhost',
-  database: 'dashboard',
-  password: 'password123',
-  port: 5432,
-})
 
 app.use(cors(corsOptions))
 app.use(express.json());
@@ -38,18 +34,19 @@ app.post("/register", cors(corsOptions), async (req, res) => {
 
         // Aggiungo l'utente al Database
         const user_obj = { user, encrypted_password };
-        let result = db.createUser(user_obj);
 
-        // Tutto è andato bene passo i dati nella RESPONSE
-        if (result == undefined) {
+        // Creazione usando supabase
+        const { error } = await supabase
+        .from('accounts')
+        .insert(user_obj);
+
+        // Se non ci sono errori aggiungi al DB
+        if(!error) {
             res.status(201).json({ user, encrypted_password })
-        }
-
-        // C'è stato qualche problema nell'inserimento nel Database
-        if (result == 1) {
+        } else if(error) {
             res.status(409);
         }
-
+    
     } catch (err) {
         console.log(err);
     }
@@ -60,46 +57,52 @@ app.post("/login", cors(corsOptions), async (req, res) => {
     // Ottengo i dati dal form
     const { user, pwd } = req.body;
 
+    
     // SQL query to fetch user details
-    const query = `SELECT * FROM accounts WHERE username = '${user}'`;
-    // execute query
-    pool.query(query, (error, results) => {
-        if (error) {
-            throw error;
-        }
-        if (results.rows.length > 0) {
-            // Verifico che la password inserita sia quella corretta
-            bcrypt.compare(pwd, results.rows[0].pwd, (err, isMatch) => {
-                if (err) {
-                    throw err;
-                }
-                // Il login ha avuto SUCCESSO
-                if (isMatch) {
-                    // Creo JWT payload
-                    const payload = {
-                        //id: results.rows[0].id,
-                        username: results.rows[0].username
-                    };
-                    // Creo il token
-                    jwt.sign(payload,
-                        process.env.TOKEN_KEY,
-                        { expiresIn: "3h" },
-                        (err, token) => {
-                            if (err) {
-                                throw err;
-                              }
-                              res.status(201).json({
-                                success: true,
-                                token: `${token}`
-                              });
-                        })
-                }else {
-                    // Il login è FALLITO
-                    res.status(401).json({ error: 'Invalid credentials' });
-                  }
-            });
-        }
-    });
+    //const query = `SELECT * FROM accounts WHERE username = '${user}'`;
+    
+    const { data, error } = await supabase
+    .from('accounts')
+    .select()
+    .eq('user', user)
+
+    let encrypted_password = '';
+    data.map(fields => encrypted_password = fields.encrypted_password);
+
+    if(!error) {
+        bcrypt.compare(pwd, encrypted_password, (err, isMatch) => {
+            if (err) {
+                throw err;
+            }
+            // Il login ha avuto SUCCESSO
+            if (isMatch) {
+                // Creo JWT payload
+                const payload = {
+                    //id: results.rows[0].id,
+                    username: user
+                };
+                // Creo il token
+                jwt.sign(payload,
+                    process.env.TOKEN_KEY,
+                    { expiresIn: "3h" },
+                    (err, token) => {
+                        if (err) {
+                            throw err;
+                          }
+                          res.status(201).json({
+                            success: true,
+                            token: `${token}`
+                          });
+                    })
+            }else {
+                // Il login è FALLITO
+                res.status(401).json({ error: 'Invalid credentials' });
+              }
+        });
+    } else if(error){
+        // Il login è FALLITO
+        res.status(401).json({ error: 'Invalid credentials' });
+    }
 });
 
 
